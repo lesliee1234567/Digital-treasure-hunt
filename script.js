2cb05de92c274d45658d748bc349ea32ece5e0a5
lesliee1234567 <43474205+lesliee1234567@users.noreply.github.com>
2019-03-07 08:35:13 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$(".sandwhich").click(function(){
+    $("#inside").show();
+});
+
+$(".watch").click(function(){
+    $(".watch").slideUp();
+    $(".blah").text("Now click on the button to watch spondgebob cook");
+    $(".blah").css(".color", ".red");
+});
+
+$(".bro").click(function(){
+    $("#done").show();
+});
+ 
+
